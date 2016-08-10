be27478d005aa4dd28f31fbccc1f5a4fc3e50e70
Anthony Jesmok <ajesmok@misix.com>
2016-08-10 16:23:49 -0500

    Adding functionality for the bot framework, dialogs.

--- app.js
+++ app.js
@@ -3,3 +3,59 @@ var restify = require('restify'),
     builder = require('botbuilder'),
     server = restify.createServer();
 
+// Setup Bot Server Instance
+server.listen(3798, function () {
+    console.log('Bot Started: %s listening to %s', server.name, server.url);
+});
+
+// Setup Our Chat Connector
+// This allows us to connect our bot to the Microsoft Bot Framework.
+// Credentials do not need to be valid until production.
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+// Create the "brains of our bot", which manages dialogs.
+var bot = new builder.UniversalBot(connector);
+// Have the ChatConnector listen for messages on an API endpoint.
+server.post('/api/messages', connector.listen());
+
+// Setup LUIS
+// Dialog URL from the Publish Screen
+var model = "https://api.projectoxford.ai/luis/v1/application?id=2a917da5-f215-4e3c-9889-76e46de08b9d&subscription-key=87d340526f9647e5a20a61a6125a4a9e";
+// Create a Dialog from the Model
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({
+    recognizers: [recognizer]
+});
+bot.dialog('/', dialog);
+
+// onDefault Handler
+// Triggered when no other intents match the user's query.
+dialog.onDefault(function (session, args) {
+    session.send("I could not understand what you said. Please try again. I may not be able to handle your request.");
+});
+
+// onBegin Handler
+// Triggered when the conversation is started
+dialog.onBegin(function (session, args) {
+    session.send("Hello there! Feel free to ask me about the Connect.Tech conference speakers, sessions, and information about the venue or hotel.");
+});
+
+// "None" Intent
+// Triggered when a match for the "None" intent is detected.
+dialog.matches('None', function (session, args) {
+    session.send("Sorry, that is something I cannot do.");
+});
+
+// "GetHotelInfo" Intent
+// Triggered when hotel information is requested.
+dialog.matches('GetHotelInfo', function (session, args) {
+    session.send("The conference hotel is the Sheraton Suites Galleria-Atlanta, located at 2844 Cobb Parkway SE. Atlanta, GA 30339. Phone number is (770) 955-3900. A link to book rooms for the conference is online.");
+});
+
+// "GetVenueInfo" Intent
+// Triggered when venue information is requested.
+dialog.matches('GetVenueInfo', function (session, args) {
+    session.send("The conference will be held at the Cobb Galleria Centre at Galleria Specialty Mall, 2 Galleria Pkwy SE, Atlanta, GA 30339. Phone number is (770) 955-8000. A map is located online at the conference website.");
+});
---
+++ scraper.js
@@ -0,0 +1,56 @@
+// IMPORTANT
+// THIS IS NOT A NODE SCRIPT
+// You must install PhantomJS and CasperJS to run this script properly.
+
+// Include Modules and Files
+var casper = require('casper').create();
+var fs = require('fs');
+var system = require('system');
+
+// Curent File Path
+var curFilePath = fs.absolute(system.args[3]).split('/');
+if (curFilePath.length > 1) {
+    curFilePath.pop();
+    curFilePath = curFilePath.join('/');
+}
+
+// Casper Tasks
+casper.start('http://connect-js.com/', function () {
+	// Create Our Output JSON
+	var output = {
+        sessions: [],
+        speakers: []
+    };
+    this.echo("Beginning Scrape of Data...");
+    this.then(function() {
+    	var pushedSpeakers = [];
+    	var sessionTitles = this.getElementsInfo('#sessions div[class="modal fade"] h4[class="modal-title"]');
+    	var sessionDescriptions = this.getElementsInfo('#sessions div[class="modal fade"] div[class="modal-body panel-body-session"]');
+    	var speakerNames = this.getElementsInfo('#sessions div[class="modal fade"] div[class="modal-footer"] > h4');
+    	var speakerArea = this.getElementsInfo('#sessions div[class="modal fade"] div[class="modal-footer"]');
+    	var speakerBios = this.getElementsInfo('#sessions div[class="modal fade"] div[class="modal-footer"] > span[class="panel-body-session-nobg"]');
+        for (var i = 0; i < sessionTitles.length; i++) {
+        	var sessionToPush = {};
+        	var speakerToPush = {};
+        	sessionToPush['title'] = sessionTitles[i].text.trim();
+        	sessionToPush['description'] = sessionDescriptions[i].text.trim();
+        	sessionToPush['speaker'] = speakerArea[i].text.trim().split('\n')[0].trim();
+        	speakerToPush['name'] = speakerNames[i].text.trim();
+        	speakerToPush['bio'] = speakerBios[i].text.trim();
+        	output.sessions.push(sessionToPush);
+        	if(pushedSpeakers.indexOf(speakerToPush['name']) < 0) {
+        		output.speakers.push(speakerToPush);
+        		pushedSpeakers.push(speakerToPush['name']);
+        	}
+        }
+    });
+    this.then(function () {
+        fs.write(curFilePath + "/data.json", JSON.stringify(output, null, 4), 'w');
+    });
+});
+
+//Run Casper, Exit on Complete
+casper.run(function () {
+    this.echo("Scrape Complete!");
+    this.exit();
+});
